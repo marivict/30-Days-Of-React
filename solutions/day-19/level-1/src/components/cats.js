bb63a3394102cd3dff59e09554a4623c0d65e2ca
Maria De La Rosa <victy-88@hotmail.com>
2021-01-16 07:33:01 -0400

    fix: add a boolean variable to the image cat

--- solutions/day-19/level-1/src/components/cats.js
+++ solutions/day-19/level-1/src/components/cats.js
@@ -1,35 +1,24 @@
 import React from 'react'
 
 const Cats = ({breeds}) => {
+    let url
     if(breeds.image === undefined || breeds.image.url === undefined){
-        return (
-            <div className="cat" keys={breeds.id}>
-                <div className="cat-background">
-                    <img src='https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg' alt={breeds.name}/>
-                </div>
-                <div className="cat-text">
-                    <h1>{breeds.name}</h1>
-                    <h3>{breeds.origin}</h3>
-                    <p><span className="cat-text-gray">Temperament: </span>{breeds.temperament}</p>
-                    <p><span className="cat-text-gray">Life Span:</span> {breeds.life_span}</p>
-                    <p><span className="cat-text-gray">Weight:</span> {breeds.weight.metric}</p>
-                    <p><span className="cat-text-gray">Description: </span>{breeds.description}</p>
-                </div>
-            </div>
-        )
-    }
+        url = 'https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg'
+    }else{
+        url= breeds.image.url
+    }    
     return(
-        <div className="cat" keys={breeds.id}>
-            <div className="cat-background">
-                <img src= {breeds.image.url} alt={breeds.name} />
+        <div className="cat-card" keys={breeds.id}>
+            <div className="cat-card-image">
+                <img src= {url} alt={breeds.name} />
             </div>
-            <div className="cat-text">
+            <div className="cat-card-description">
                 <h1>{breeds.name}</h1>
                 <h3>{breeds.origin}</h3>
-                <p><span className="cat-text-gray">Temperament: </span>{breeds.temperament}</p>
-                <p><span className="cat-text-gray">Life Span:</span> {breeds.life_span}</p>
-                <p><span className="cat-text-gray">Weight:</span> {breeds.weight.metric}</p>
-                <p><span className="cat-text-gray">Description: </span>{breeds.description}</p>
+                <p><span className="cat-card-description-color-gray">Temperament: </span>{breeds.temperament}</p>
+                <p><span className="cat-card-description-color-gray">Life Span:</span> {breeds.life_span}</p>
+                <p><span className="cat-card-description-color-gray">Weight:</span> {breeds.weight.metric}</p>
+                <p><span className="cat-card-description-color-gray">Description: </span>{breeds.description}</p>
             </div>
         </div>
     )
