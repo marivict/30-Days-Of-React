92ac1f4df30e59367a24826713a8712c0076cb45
Maria De La Rosa <m.delarosa@gbh.com.do>
2021-03-18 21:23:17 -0400

    feat: add new folder structure

---
+++ solutions/day-24/level-1/src/components/Search/search.js
@@ -0,0 +1,20 @@
+import React,{useState, useEffect} from 'react'
+import Country from '../Country/country'
+
+const Search = ({search}) => {
+
+  const [valueSearch, setValue] = useState('')
+
+  const handleInput = (e) => {
+    const query = e.target.value
+    setValue(query)
+    search(valueSearch)
+  }
+  return(
+      <div className="search-container">
+          <input className="search" type="text" value={valueSearch} onChange={handleInput} placeholder="Search countries by name, city and language" />
+      </div>
+
+  )
+}
+export default Search
