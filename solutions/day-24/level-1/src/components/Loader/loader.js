00410a8979e73219cdea4e886adc71f43a5b630d
Maria De La Rosa <m.delarosa@gbh.com.do>
2021-03-21 22:41:40 -0400

    fix: change class for className

--- solutions/day-24/level-1/src/components/Loader/loader.js
+++ solutions/day-24/level-1/src/components/Loader/loader.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Loader = () =>{
     const newDiv = new Array(0,1,2,3,4,5,6,7,8,9,10,11)
     return(
-        <div class="loader">
-            <div class="lds-ring">
-                <div class="lds-spinner">
+        <div className="loader">
+            <div className="lds-ring">
+                <div className="lds-spinner">
                     {newDiv.map(newDiv => (
                         <div></div>
                     ))}
